c63e0de55c08a63369f2f79fc26b11d866b54073
Nenad <ahhhhmed@gmail.com>
2017-05-13 12:33:11 +0200

    Added setup file.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='door',
+    version='0.0',
+    packages=[''],
+    url='https://github.com/MilicaSelakovic/door',
+    license='',
+    author='Milica',
+    author_email='milica1793@gmail.com',
+    description='class project',
+    install_requires=[
+        'flask',
+        'audiogen',
+        'pyaudio'
+    ]
+)
