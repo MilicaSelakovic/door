382c3e3bd959564ba07ab67025559d6297e2b2ff
milica <milica1793@gmail.com>
2017-05-15 16:46:17 +0200

    spectrogram changed

--- door/static/audio.js
+++ door/static/audio.js
@@ -69,7 +69,7 @@ Visualizer.prototype = {
         analyser_node.smoothingTimeConstant = 0.85;
         analyser_node.minDecibels = -90;
         analyser_node.maxDecibels = -10;
-        analyser_node.fftSize = BUFF_SIZE_RENDERER;
+        analyser_node.fftSize = 2048;
 
         distortion = this.audioContext.createWaveShaper();
         biquadFilter = this.audioContext.createBiquadFilter();
@@ -95,59 +95,92 @@ Visualizer.prototype = {
         // };
 },
     _drawSpectrum: function(analyser) {
-        var that = this,
-            canvas = document.getElementById('canvas'),
-            cwidth = canvas.width,
-            cheight = canvas.height - 2,
-            meterWidth = 10, //width of the meters in the spectrum
-            gap = 2, //gap between meters
-            capHeight = 2,
-            capStyle = '#fff',
-            meterNum = 800 / (10 + 2), //count of the meters
-            capYPositionArray = []; ////store the vertical position of hte caps for the preivous frame
-        ctx = canvas.getContext('2d'),
-        gradient = ctx.createLinearGradient(0, 0, 0, 300);
-        gradient.addColorStop(1, '#0f0');
-        gradient.addColorStop(0.5, '#ff0');
-        gradient.addColorStop(0, '#f00');
-        var drawMeter = function() {
-            var array = new Uint8Array(analyser.frequencyBinCount);
-            analyser.getByteFrequencyData(array);
-            if (that.status === 0) {
-                //fix when some sounds end the value still not back to zero
-                for (var i = array.length - 1; i >= 0; i--) {
-                    array[i] = 0;
-                };
-                allCapsReachBottom = true;
-                for (var i = capYPositionArray.length - 1; i >= 0; i--) {
-                    allCapsReachBottom = allCapsReachBottom && (capYPositionArray[i] === 0);
-                };
-                if (allCapsReachBottom) {
-                    cancelAnimationFrame(that.animationId); //since the sound is stoped and animation finished, stop the requestAnimation to prevent potential memory leak,THIS IS VERY IMPORTANT!
-                    return;
-                };
-            };
-            var step = Math.round(array.length / meterNum); //sample limited data from the total array
-            ctx.clearRect(0, 0, cwidth, cheight);
-            for (var i = 0; i < meterNum; i++) {
-                var value = array[i * step];
-                if (capYPositionArray.length < Math.round(meterNum)) {
-                    capYPositionArray.push(value);
-                };
-                ctx.fillStyle = capStyle;
-                //draw the cap, with transition effect
-                if (value < capYPositionArray[i]) {
-                    ctx.fillRect(i * 12, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
-                } else {
-                    ctx.fillRect(i * 12, cheight - value, meterWidth, capHeight);
-                    capYPositionArray[i] = value;
-                };
-                ctx.fillStyle = gradient; //set the filllStyle to gradient for a better look
-                ctx.fillRect(i * 12 /*meterWidth+gap*/ , cheight - value + capHeight, meterWidth, cheight); //the meter
-            }
-            that.animationId = requestAnimationFrame(drawMeter);
+      analyser.fftSize = 256;
+      canvas = document.getElementById('canvas');
+      cwidth = canvas.width;
+      cheight = canvas.height - 2;
+      canvasCtx = canvas.getContext('2d');
+      var bufferLength = analyser.frequencyBinCount;
+      console.log(bufferLength);
+      var dataArray = new Uint8Array(bufferLength);
+
+      canvasCtx.clearRect(0, 0, cwidth, cheight);
+
+      function draw() {
+        drawVisual = requestAnimationFrame(draw);
+
+        analyser.getByteFrequencyData(dataArray);
+
+        canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+        canvasCtx.fillRect(0, 0, cwidth, cheight);
+
+        var barWidth = (cwidth / bufferLength) * 2.5;
+        var barHeight;
+        var x = 0;
+
+        for(var i = 0; i < bufferLength; i++) {
+          barHeight = dataArray[i];
+
+          canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+          canvasCtx.fillRect(x,cheight-barHeight/2,barWidth,barHeight/2);
+
+          x += barWidth + 1;
         }
-        this.animationId = requestAnimationFrame(drawMeter);
+      };
+      draw();
+      //   var that = this,
+      //       canvas = document.getElementById('canvas'),
+      //       cwidth = canvas.width,
+      //       cheight = canvas.height - 2,
+      //       meterWidth = 10, //width of the meters in the spectrum
+      //       gap = 2, //gap between meters
+      //       capHeight = 2,
+      //       capStyle = '#fff',
+      //       meterNum = 800 / (10 + 2), //count of the meters
+      //       capYPositionArray = []; ////store the vertical position of hte caps for the preivous frame
+      //   ctx = canvas.getContext('2d'),
+        // gradient = ctx.createLinearGradient(0, 0, 0, 300);
+        // gradient.addColorStop(1, '#0f0');
+        // gradient.addColorStop(0.5, '#ff0');
+        // gradient.addColorStop(0, '#f00');
+        // var drawMeter = function() {
+        //     var array = new Uint8Array(analyser.frequencyBinCount);
+        //         for (var i = array.length - 1; i >= 0; i--) {
+        //     analyser.getByteFrequencyData(array);
+        //     if (that.status === 0) {
+        //         //fix when some sounds end the value still not back to zero
+        //             array[i] = 0;
+        //         };
+        //         allCapsReachBottom = true;
+        //         for (var i = capYPositionArray.length - 1; i >= 0; i--) {
+        //             allCapsReachBottom = allCapsReachBottom && (capYPositionArray[i] === 0);
+        //         };
+        //         if (allCapsReachBottom) {
+        //             cancelAnimationFrame(that.animationId); //since the sound is stoped and animation finished, stop the requestAnimation to prevent potential memory leak,THIS IS VERY IMPORTANT!
+        //             return;
+        //         };
+        //     };
+        //     var step = Math.round(array.length / meterNum); //sample limited data from the total array
+        //     ctx.clearRect(0, 0, cwidth, cheight);
+        //     for (var i = 0; i < meterNum; i++) {
+        //         var value = array[i * step];
+        //         if (capYPositionArray.length < Math.round(meterNum)) {
+        //             capYPositionArray.push(value);
+        //         };
+        //         ctx.fillStyle = capStyle;
+        //         //draw the cap, with transition effect
+        //         if (value < capYPositionArray[i]) {
+        //             ctx.fillRect(i * 12, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
+        //         } else {
+        //             ctx.fillRect(i * 12, cheight - value, meterWidth, capHeight);
+        //             capYPositionArray[i] = value;
+        //         };
+        //         ctx.fillStyle = gradient; //set the filllStyle to gradient for a better look
+        //         ctx.fillRect(i * 12 /*meterWidth+gap*/ , cheight - value + capHeight, meterWidth, cheight); //the meter
+        //     }
+        //     that.animationId = requestAnimationFrame(drawMeter);
+        // }
+        // this.animationId = requestAnimationFrame(drawMeter);
     },
     _audioEnd: function(instance) {
         if (this.forceStop) {
